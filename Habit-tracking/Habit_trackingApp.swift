7d6886e049db6cacfe14b1a6666590fb9def8a6c
Vinicius Ledis <161168846+viniciusledis@users.noreply.github.com>
2024-03-19 10:35:26 -0300

    Initial Commit

---
+++ Habit-tracking/Habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackingApp.swift
+//  Habit-tracking
+//
+//  Created by Vinicius Ledis on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
